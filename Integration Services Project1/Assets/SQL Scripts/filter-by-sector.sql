ebfe7dae2d6f560052b923a73fcf8584d0ec1a0d
Sumeet Sharma <sharmasumeet1987@gmail.com>
2020-12-20 23:47:07 -0500

    Filter by NPM and Market Cap

--- Integration Services Project1/Assets/SQL Scripts/filter-by-sector.sql
+++ Integration Services Project1/Assets/SQL Scripts/filter-by-sector.sql
@@ -1,5 +1,7 @@
 --select * from MARKET.TempDataBankNew
 
+
+
 sp_rename '[MARKET].[TempDataBankNew]', 'TempDataBank'
 
 SELECT DB.CompanyName
@@ -50,4 +52,33 @@ SELECT DB.CompanyName
 ,DB.Sector
 FROM MARKET.TempDataBank DB
 ORDER BY DB.Sector ASC
+
+
+--*********************************Filter by NPM and Market Cap < 100 cr ***************************
+SELECT DB.CompanyName
+, DB.Sales
+,DB.NetProfit
+, DB.Y2DSales
+,DB.Y2DNetProfit
+, MARKET.PercentYield(DB.NetProfit, DB.Sales) AS  NPM
+, MARKET.PercentYield(DB.Y2DNetProfit, DB.Y2DSales) AS Y2DNPM
+, MARKET.PercentYield(DB.Dividend, DB.Price) AS DividendYield
+, DB.DebtEquityRatio
+,DB.PERatio
+,DB.MarketCap
+,DB.EPS
+,CAST(ROUND((DB.RONW), 2,1) AS NUMERIC(36,2)) AS RONW
+,DB.BookValue
+,DB.CurrentEquity
+,DB.FaceValue
+,(DB.Price) AS Price
+,(DB.FiftyTwoWeekHigh) AS FiftyTwoWeekHigh
+,(DB.FiftyTwoWeekLow) AS FiftyTwoWeekLow
+,Market.ToNumeric(DB.CurrentEquity/DB.FaceValue) AS NoOfSharesOutstanding
+,DB.Sector
+FROM MARKET.TempDataBank DB
+WHERE MARKET.PercentYield(DB.NetProfit, DB.Sales)  > 15 
+AND   MARKET.PercentYield(DB.Y2DNetProfit, DB.Y2DSales)   > 15 
+ AND MarketCap BETWEEN 150 AND  250
+ORDER BY MarketCap DESC
 
